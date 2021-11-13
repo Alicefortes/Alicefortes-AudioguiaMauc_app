bc3b3df189f771a3eafbe6eff4bab1e27f71c745
vitor estevam <vitorestevam02@gmail.com>
2021-11-13 00:13:34 -0300

    change api address

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = "https://mauc-api.herokuapp.com/api/"
+const apiUrl = "https://mauc-dashboard.herokuapp.com/api/"
 
 export async function fetchGuides(){
     return axios.get(apiUrl+"guides/").then(resp => resp.data)
